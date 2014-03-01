4c3c3277b9513675d12f6dd465e6bca9664b1e01
Pronin Alexander <pronin.alx@gmail.com>
2014-03-01 16:55:00 +0200

    Initial Commit

---
+++ 20OutletsTest/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  20OutletsTest
+//
+//  Created by Aleksandr Pronin on 01.03.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
---
+++ 20OutletsTestTests/_0OutletsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  _0OutletsTestTests.m
+//  20OutletsTestTests
+//
+//  Created by Aleksandr Pronin on 01.03.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0OutletsTestTests : XCTestCase
+
+@end
+
+@implementation _0OutletsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
